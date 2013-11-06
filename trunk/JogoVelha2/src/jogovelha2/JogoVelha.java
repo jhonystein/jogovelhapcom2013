c7bf0efa4e70b119d8def23df05ffa9e433d1708
jhonystein@gmail.com <jhonystein@gmail.com@3ade4a79-2588-75b5-d733-304e54e03f4d>
2013-11-06 23:23:35 +0000

    Inicialização do projeto

---
+++ trunk/JogoVelha2/src/jogovelha2/JogoVelha.java
@@ -0,0 +1,71 @@
+package jogovelha2;
+
+/**
+ *
+ * @author senai-sc
+ */
+public class JogoVelha {
+
+    private String[][] tabuleiro;
+    
+    private int jogadas;
+    
+    private String jogadorAtual;
+    
+    public JogoVelha() {
+        tabuleiro = new String[][]{{"","",""},{"","",""},{"","",""}};
+        jogadas = 0;
+        jogadorAtual = "X";
+    }
+    
+    public void preencherTabuleiro(int x, int y) {
+        if (tabuleiro[x][y].equals("")) {
+            tabuleiro[x][y] = jogadorAtual;
+            if (!verificarEmpate() && !verificarGanhador()) {
+                // trocar jogador autal
+                jogadorAtual = jogadorAtual.equals("X") ? "O" : "X";
+                jogadas++;
+            }
+        }
+    }
+    
+    public String retornarJogadorAtual() {
+        return jogadorAtual;
+    }
+    
+    public boolean verificarEmpate() {
+        if (!verificarGanhador() && jogadas >= 9) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean verificarGanhador() {
+        // verificar linhas
+        for(int linha = 0; linha < tabuleiro.length; linha++) {
+            if (!tabuleiro[linha][0].isEmpty() && 
+                    tabuleiro[linha][0].equals(tabuleiro[linha][1]) &&
+                    tabuleiro[linha][1].equals(tabuleiro[linha][2])) {
+                return true;
+            }
+        }
+        for (int coluna = 0; coluna < tabuleiro.length; coluna++) {
+            if (!tabuleiro[0][coluna].isEmpty() && 
+                    tabuleiro[0][coluna].equals(tabuleiro[1][coluna]) &&
+                    tabuleiro[1][coluna].equals(tabuleiro[2][coluna])) {
+                return true;
+            }
+        }
+        if (!tabuleiro[0][0].isEmpty() &&
+                tabuleiro[0][0].equals(tabuleiro[1][1]) &&
+                tabuleiro[1][1].equals(tabuleiro[2][2])) {
+            return true;
+        }
+        if (!tabuleiro[0][2].isEmpty() &&
+                tabuleiro[0][2].equals(tabuleiro[1][1]) &&
+                tabuleiro[1][1].equals(tabuleiro[2][0])) {
+            return true;
+        }
+        return false;
+    }
+}
